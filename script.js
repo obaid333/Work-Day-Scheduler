d73595498643c40f5a2e82ec1967260c2b050676
Obaid Gulzar <obaid3337@googlemail.com>
2024-01-22 05:55:25 +0000

    array with objects of name, time, active and userTask keys created

--- script.js
+++ script.js
@@ -1,4 +1,61 @@
-let hoursArray = [];
-let datesArray = [];
+let allTimesArray = [
+    {
+        name: "9am",
+        time: "9:00",
+        active: true,
+        userTask: ""
+    },
+    {
+        name: "10am",
+        time: "10:00",
+        active: true,
+        userTask: ""
+    },
+    {
+        name: "11am",
+        time: "11:00",
+        active: true,
+        userTask: ""
+    },
+    {
+        name: "12pm",
+        time: "12:00",
+        active: true,
+        userTask: ""
+    },
+    {
+        name: "1pm",
+        time: "13:00",
+        active: true,
+        userTask: ""
+    },
+    {
+        name: "2pm",
+        time: "14:00",
+        active: true,
+        userTask: ""
+    },
+    {
+        name: "3pm",
+        time: "15:00",
+        active: true,
+        userTask: ""
+    },
+    {
+        name: "4pm",
+        time: "16:00",
+        active: true,
+        userTask: ""
+    },
+    {
+        name: "5pm",
+        time: "17:00",
+        active: true,
+        userTask: ""
+    }
+];
+
+
+
 
 
